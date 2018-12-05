51bbd091665dd9e67be9ab8388c19b5601945451
Gregor Große-Bölting <gregorbg@web.de>
2018-12-05 19:49:40 +0100

    Shorter, nicer and faster solution... with code reuse

--- day3_2.rkt
+++ day3_2.rkt
@@ -1,23 +1,7 @@
 #lang racket
 
+(require "day3_1.rkt")
 (require threading)
-
-(define (elf-squares offset-x offset-y width height)
-  (list->set
-   (foldl append (list)
-          (for/list ([x (in-range offset-x (+ offset-x width))])
-            (for/list ([y (in-range offset-y (+ offset-y height))])
-              (cons x y))))))
-
-(define (intersect-helper a b)
-  (if (equal? a b)
-      (set)
-      (set-intersect a b)))
-
-(define (elf-collisions all-squares elf-square)
-  (~>> all-squares
-       (map (curry intersect-helper elf-square))
-       (foldl set-union (set))))
 
 (define (parse-input input-str)
   (~>> (string-split input-str #px"\\@|,|:|x")
@@ -37,15 +21,14 @@
             (add1 (- (apply max scd) min-scd)))))
  
 (define (solve-puzzle input)
-  (let* ([all-squares (~>> input
+  (let* ([overlap (~>> (solve-puzzle* input)
+                       (map first)
+                       (list->set))]
+         [target-dims (~>> input
                           (map parse-input)
-                          (map (λ (e) (apply elf-squares e))))]
-         [overlap (~>> all-squares
-                       (map (curry elf-collisions all-squares))
-                       (foldl set-union (set)))]
-         [target-dims (~>> all-squares
-                           (filter (lambda (e) (set-empty? (set-intersect e overlap))))
-                           (revert-to-dim))])
+                          (map (λ (e) (list->set (apply elf-squares e))))
+                          (filter (lambda (e) (set-empty? (set-intersect e overlap))))
+                          (revert-to-dim))])
     (first (filter (λ (e) (string-contains? e target-dims)) input))))
 
 (define test-input 
