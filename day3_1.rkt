56b918c97daaa9a6bddac3799e75c78b414bc4af
Gregor Große-Bölting <gregorbg@web.de>
2018-12-05 19:39:48 +0100

    Much faster and nicer solution for day 3

--- day3_1.rkt
+++ day3_1.rkt
@@ -2,38 +2,33 @@
 
 (require threading)
 
+(provide elf-squares parse-input solve-puzzle*)
+
 (define (elf-squares offset-x offset-y width height)
-  (list->set
-   (foldl append (list)
-          (for/list ([x (in-range offset-x (+ offset-x width))])
-            (for/list ([y (in-range offset-y (+ offset-y height))])
-              (cons x y))))))
-
-(define (intersect-helper a b)
-  (if (equal? a b)
-      (set)
-      (set-intersect a b)))
-
-(define (elf-collisions all-squares elf-square)
-  (~>> all-squares
-       (map (curry intersect-helper elf-square))
-       (foldl set-union (set))))
+  (foldl append (list)
+         (for/list ([x (in-range offset-x (+ offset-x width))])
+           (for/list ([y (in-range offset-y (+ offset-y height))])
+             (cons x y)))))
 
 (define (parse-input input-str)
   (~>> (string-split input-str #px"\\@|,|:|x")
        (rest)
        (map (compose string->number string-trim))))
 
+(define (solve-puzzle* input)
+  (~>> input
+       (map parse-input)
+       (map (λ (e) (apply elf-squares e)))
+       (foldl append '())
+       (group-by identity)
+       (filter (λ (v) (> (length v) 1)))))
+
 (define (solve-puzzle input)
-  (let ([all-squares (~>> input
-                          (map parse-input)
-                          (map (λ (e) (apply elf-squares e))))])
-    (~>> all-squares
-         (map (curry elf-collisions all-squares))
-         (foldl set-union (set))
-         (set-count))))
+  (length (solve-puzzle* input)))
 
 (define test-input 
   (list "#1 @ 1,3: 4x4" "#1 @ 3,1: 4x4" "#3 @ 5,5: 2x2"))
 
 (= 4 (solve-puzzle test-input))
+
+; (solve-puzzle (file->lines "day3_input.txt"))
