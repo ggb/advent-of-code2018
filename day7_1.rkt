491b94c45277e9b423e64d5aa6cdb4979c568383
ggb <gregorbg@web.de>
2018-12-07 16:47:12 +0100

    Finally... finished day7, problem 1

--- day7_1.rkt
+++ day7_1.rkt
@@ -4,7 +4,7 @@
 
 (define (not-available? pairs target)
   (~>> pairs
-       (filter (lambda (p) (equal? target (cdr p))))
+       (filter (λ (p) (equal? target (cdr p))))
        (map car)
        (length)
        (< 1)
@@ -12,38 +12,38 @@
 
 (define (find-next pairs start)
   (sort 
-   (filter (lambda (p) (equal? start (car p))) pairs)
+   (filter (λ (p) (equal? start (car p))) pairs)
    #:key cdr
    string<?))
 
 (define (find-start pairs)
-  (first
+  (sort
    (set-subtract
     (map car pairs)
-    (map cdr pairs))))
+    (map cdr pairs))
+   string<?))
 
 (define (solve pairs source visited open)
-  (begin
-    (println visited)
-    (if (empty? pairs)
-        visited
-        (let* ([nexts (find-next pairs source)]
-               [next-pairs (set-subtract pairs nexts)]
-               [targets (filter (curry not-available? pairs) (map cdr nexts))])
-          (if (empty? targets)
-              (solve next-pairs (first open) (cons (first open) visited) (rest open))
-              (solve next-pairs (first targets) (cons (first targets) visited) (append (rest targets) open)))))))
+  (if (empty? pairs)
+      visited
+      (let* ([nexts (find-next pairs source)]
+             [next-pairs (set-subtract pairs nexts)]
+             [targets (filter (curry not-available? pairs) (map cdr nexts))])
+        (if (empty? targets)
+            (solve next-pairs (first open) (cons (first open) visited) (rest open))
+            (solve next-pairs (first targets) (cons (first targets) visited) (append (rest targets) open))))))
 
 (define (solve-puzzle input)
   (let* ([pairs (parse-input input)]
-         [start (find-start pairs)])
+         [open (find-start pairs)]
+         [start (first open)])
     (string-join
-     (reverse (solve pairs start (list start) (list)))
+     (reverse (solve pairs start (list start) (rest open)))
      "")))
 
 (define (parse-input input)
   (map
-   (lambda (i)
+   (λ (i)
      (let ([m (regexp-match #px"Step (\\w) must be finished before step (\\w) can begin." i)])
        (cons (second m) (third m))))
    input))
@@ -58,3 +58,5 @@
         "Step F must be finished before step E can begin."))
 
 (equal? "CABDFE" (solve-puzzle test-input))
+
+; (solve-puzzle (file->lines "day7_input.txt"))
