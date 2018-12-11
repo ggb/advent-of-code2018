482e5722ab83fcb0310fe18a76cba42b9c3b547e
Gregor Große-Bölting <gregorbg@web.de>
2018-12-11 22:04:25 +0100

    Update solution for second task on day 11

--- day11_2.rkt
+++ day11_2.rkt
@@ -1,6 +1,5 @@
 #lang racket
 
-(require pmap)
 (require "day11_1.rkt")
 
 (define (solution-for-square n)
@@ -11,6 +10,10 @@
   (list (range 1 76) (range 76 151) (range 151 226) (range 226 301)))
 
 (define (solve-puzzle)
-  (argmax (pmapf (lambda (chunk) (argmax first (map solution-for-square chunk))) chunks)))
+  (let ([f1 (lambda () (argmax first (map solution-for-square (first chunks))))]
+        [f2 (future (lambda () (argmax first (map solution-for-square (second chunks)))))]
+        [f3 (future (lambda () (argmax first (map solution-for-square (third chunks)))))]
+        [f4 (future (lambda () (argmax first (map solution-for-square (fourth chunks)))))])
+    (argmax first (list (f1) (touch f2) (touch f3) (touch f4)))))
 
-(solve-puzzle)
+;(solve-puzzle)
