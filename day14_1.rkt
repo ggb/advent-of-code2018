7f5799f3fabd6fa2840ac89a4da7aaf0a6b83b2a
Gregor Große-Bölting <gregorbg@web.de>
2018-12-15 00:08:52 +0100

    Change hash-map to gvector

--- day14_1.rkt
+++ day14_1.rkt
@@ -1,26 +1,29 @@
 #lang racket
 
 (require threading)
+(require data/gvector)
 
 (define (create-recipes positions x y len i max-iter)
   (if (= i max-iter)
       positions
-      (let* ([m (hash-ref positions x)]
-             [n (hash-ref positions y)]
+      (let* ([m (gvector-ref positions x)]
+             [n (gvector-ref positions y)]
              [new-m (quotient (+ m n) 10)]
              [new-n (modulo (+ m n) 10)]
-             [new-positions (if (= 0 new-m)
-                                (hash-set positions len new-n)
-                                (hash-set (hash-set positions len new-m) (+ len 1) new-n))]
+             [_ (if (= 0 new-m)
+                    (gvector-insert! positions len new-n)
+                    (begin
+                      (gvector-insert! positions len new-m)
+                      (gvector-insert! positions (+ len 1) new-n)))]
              [new-len (if (= 0 new-m) (+ len 1) (+ len 2))]
              [new-x (modulo (+ x (add1 m)) new-len)]
              [new-y (modulo (+ y (add1 n)) new-len)])
-        (create-recipes new-positions new-x new-y new-len (add1 i) max-iter))))
+        (create-recipes positions new-x new-y new-len (add1 i) max-iter))))
 
 (define (solve-puzzle recipe-count n)
-  (let ([recipes (create-recipes #hash((0 . 3) (1 . 7)) 0 1 2 0 (+ recipe-count n))])
+  (let ([recipes (create-recipes (gvector 3 7) 0 1 2 0 (+ recipe-count n))])
     (string-join (~>> (range recipe-count (+ recipe-count n))
-                      (map (curry hash-ref recipes))
+                      (map (curry gvector-ref recipes))
                       (map number->string)) "")))
 
-; (time (solve-puzzle 190221 10))
+(time (solve-puzzle 20268576 10))
