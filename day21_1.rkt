c21847ab98f0e9527e64a4aa519bc67bcd078d77
Gregor Große-Bölting <gregorbg@web.de>
2018-12-21 23:52:30 +0100

    Solution for part 1 on day 21

---
+++ day21_1.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(require threading)
+
+; reg-a: Register 1
+; reg-b: Register 3
+; reg-c: Register 4
+(define (elf-loop reg-a reg-b)
+  (let* ([reg-c (bitwise-and reg-b 255)]            ; line 08
+         [new-reg-a (~>> (+ reg-a reg-c)            ; line 09
+                         (bitwise-and 16777215)     ; line 10
+                         (* 65899)                  ; line 11
+                         (bitwise-and 16777215))])  ; line 12
+    (if (< reg-b 256)                               ; line 13
+        new-reg-a
+        (elf-loop new-reg-a (quotient reg-b 256)))))
+
+(define (solve-puzzle inp)
+  (elf-loop inp 65536))
+
+; Look at instruction 07 for input value:
+(solve-puzzle 10905776) 
