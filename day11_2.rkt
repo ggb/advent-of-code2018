dd20d32b0193dbce5983c1194a02de3842f47404
Gregor Große-Bölting <gregorbg@web.de>
2018-12-24 11:12:31 +0100

    Solve day 11, part 2

--- day11_2.rkt
+++ day11_2.rkt
@@ -3,17 +3,13 @@
 (require "day11_1.rkt")
 
 (define (solution-for-square n)
-  (let ([solution (time (solve-puzzle* 4842 n))])
+  (let ([solution (solve-puzzle* n)])
     (list (cdr solution) (car solution) n)))
 
-(define chunks
-  (list (range 1 76) (range 76 151) (range 151 226) (range 226 301)))
-
 (define (solve-puzzle)
-  (let ([f1 (lambda () (argmax first (map solution-for-square (first chunks))))]
-        [f2 (future (lambda () (argmax first (map solution-for-square (second chunks)))))]
-        [f3 (future (lambda () (argmax first (map solution-for-square (third chunks)))))]
-        [f4 (future (lambda () (argmax first (map solution-for-square (fourth chunks)))))])
-    (argmax first (list (f1) (touch f2) (touch f3) (touch f4)))))
+  (define max-square (argmax first (map solution-for-square (range 1 300))))
+  (define origin (second max-square))
+  (define size (third max-square))
+  (printf "~a,~a,~a" (first origin) (second origin) size))
 
 ;(solve-puzzle)
